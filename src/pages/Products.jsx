8e4ee461098bb99574b0208f9fe87c7d057e864d
shibsankarBera <shibu900099@gmail.com>
2026-04-10 01:03:50 +0530

    added products page and fetched data from api

---
+++ src/pages/Products.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then(res => res.json())
+      .then(data => setProducts(data));
+  }, []);
+
+  return (
+    <div className="p-5">
+      <h1 className="text-2xl font-bold mb-5">Products</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+        {products.map((item) => (
+          <div key={item.id} className="border p-4 rounded shadow">
+            <img src={item.image} className="h-40 mx-auto" />
+            <h2 className="font-semibold mt-2">{item.title}</h2>
+            <p className="text-green-600">${item.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Products;
